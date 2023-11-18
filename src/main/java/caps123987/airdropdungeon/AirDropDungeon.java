fe49026fae08c219d915bedf1f2d15f2666b932e
CAPS123987 <hnilickakuba@gmail.com>
2023-11-18 11:31:12 +0100

    AirDrop

---
+++ src/main/java/caps123987/airdropdungeon/AirDropDungeon.java
@@ -0,0 +1,17 @@
+package caps123987.airdropdungeon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AirDropDungeon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
