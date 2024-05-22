fde035aa730152639b50bb9f46e05c8bb575be15
CAPS123987 <87692752+CAPS123987@users.noreply.github.com>
2024-05-22 18:25:24 +0200

    Delete test2.bat

--- test2.bat
+++
@@ -1,3 +0,0 @@
-xcopy "C:\Users\hnili\Desktop\kill get money\AirDropDungeon\target\AirDropDungeon-1.0-SNAPSHOT.jar" "C:\Users\hnili\Desktop\kill get money\DungeonGenerator\server\plugins\AirDropDungeon-1.0-SNAPSHOT.jar" /U /Y
-cd "C:\Users\hnili\Desktop\kill get money\DungeonGenerator\server"
-start.bat
